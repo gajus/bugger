f89660460d18df0bd189142a20fc84f4ca962391
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-04-14 06:28:44 +0000

    0.2.0

--- src/inc/autoload.php
+++ src/inc/autoload.php
@@ -20,27 +20,23 @@ $GLOBALS['gajus']['bugger'] = [];
 register_shutdown_function(function () {
 	$stack = \Gajus\Bugger\Bugger::getStack();
 
-	require __DIR__ . '/template.php';
+	if ($stack) {
+		if (php_sapi_name() === 'cli') {
+			throw new \Gajus\Bugger\Exception\LogicException('Bugger does not work in CLI environment.');
+		} else {
+			while (ob_get_level()) {
+				ob_end_clean();
+			}
 
-	/*if (!isset($GLOBALS['gajus']['bump']) && !isset($GLOBALS['gajus']['mump'])) {
+			if (!headers_sent()) {
+				header('Content-Type: text/html; charset="UTF-8"', true);
+			}
+		}
+
+		require __DIR__ . '/template.php';
+	} else {
 		return;
 	}
-
-	if (php_sapi_name() !== 'cli') {
-		while (ob_get_level()) {
-			ob_end_clean();
-		}
-
-		if (!headers_sent()) {
-			header('Content-Type: text/html; charset="UTF-8"', true);
-		}
-	} else {
-		throw new \Gajus\Bugger\Exception\LogicException('Bugger does not work in CLI environment.');
-	}
-
-	$response = $GLOBALS['gajus']['bump'];
-
-	require __DIR__ . '/template.php';*/
 });
 
 require __DIR__ . '/../Bugger.php';
