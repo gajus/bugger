c23b59cb3888cf5f6742c77e3759fd46514be142
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-04-14 06:16:10 +0000

    0.2.0

--- src/Exception/LogicException.php
+++ src/Exception/LogicException.php
@@ -6,7 +6,6 @@ namespace Gajus\Bump\Exception;
  *
  * @link https://github.com/gajus/bump for the canonical source repository
  * @license https://github.com/gajus/bump/blob/master/LICENSE BSD 3-Clause
- * @codeCoverageIgnore
  */
 class LogicException extends BumpException {
 	
