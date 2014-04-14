4610159811d05b366bf593842a0d914858b08db0
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-04-14 13:00:47 +0000

    Learned the hard way that jQuery needs to be in the package. Developing localhost.

--- demo/stack.php
+++ demo/stack.php
@@ -1,16 +1,41 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-
+/*echo 'foo';
+stack('d');
 echo 'foo';
-
-stack('test');
-
+stack('e');
 echo 'foo';
-
-stack('test');
-
+stack('f');
 echo 'foo';
-
-stack('test');
-
+stack('g');
 echo 'foo';
+stack('h');
+echo 'foo';*/
+/*echo 'foo';
+stack('d');
+echo 'foo';
+stack('e');
+echo 'foo';
+stack('f');
+echo 'foo';
+stack('g');
+echo 'foo';
+stack('h');
+echo 'foo';*/
+echo 'foo';
+stack('a');
+echo 'foo';
+stack('b');
+echo 'foo';
+stack('c');
+/*echo 'foo';
+stack('d');
+echo 'foo';
+stack('e');
+echo 'foo';
+stack('f');
+echo 'foo';
+stack('g');
+echo 'foo';
+stack('h');
+echo 'foo';*/
--- src/inc/template.php
+++ src/inc/template.php
@@ -13,38 +13,47 @@
 	<?=file_get_contents(__DIR__ . '/static/css/bugger.css')?>
 	</style>
 
-	<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-
 	<script>
+	<?=file_get_contents(__DIR__ . '/static/js/jquery-2.1.0.min.js')?>
 	<?=file_get_contents(__DIR__ . '/static/js/bugger.js')?>
 
 	<?=file_get_contents(__DIR__ . '/static/js/syntaxhighlighter/scripts/shCore.js')?>
 	<?=file_get_contents(__DIR__ . '/static/js/syntaxhighlighter/scripts/shBrushPhp.js')?>
 	</script>
 </head>
+<?php
+$display_number_of_lines = 10;
+?>
 <body>
-	<?php foreach ($stack as $trace):?>
-	<ol class="stack">
-		<?php foreach ($trace as $t):?>
-		<li>
-			<?php if (isset($t['file'])):
+	<div id="bugger">
+		<?php foreach ($stack as $trace):?>
+		<ol class="stack">
+			<?php foreach ([array_shift($trace)] as $t):?>
+			<li>
+				<?php
+				if (isset($t['file'])):
 
-			$file = file($t['file']);
-			$file = array_slice($file, max($t['line'] - 3, 0), 5);
-			$file = array_map('rtrim', $file);
-			$file = array_map(function ($e) { return $e ? $e : '	'; }, $file);
-			?>
-			<div class="location">
-				<span class="file">../<?=explode('/', mb_substr($t['file'], -80), 2)[1]?></span>:<span class="line"><?=$t['line']?></span>
-			</div>
-			<pre class="brush: php; highlight: <?=$t['line']?>; first-line: <?=max($t['line'] - 2, 1)?>;"><?=htmlspecialchars(implode($file, "\n"))?></pre>
-			<?php endif;?>
-			
-			<pre class="brush: php;"><?=$t['args_dump']?></pre>
-		</li>
+				$file = file($t['file']);
+				$file = array_map('rtrim', $file);
+
+				$number_of_line_start = max($t['line'] - $display_number_of_lines / 2, 0);
+				#$number_of_line_end = $display_number_of_lines - ($t['line'] - $number_of_line_start);
+
+				$file = array_slice($file, $number_of_line_start, $display_number_of_lines);
+				$file = array_map(function ($e) { return $e ? $e : '	'; }, $file);
+				?>
+				<div class="location">
+					<span class="file">../<?=explode('/', mb_substr($t['file'], -80), 2)[1]?></span>:<span class="line"><?=$t['line']?></span>
+				</div>
+				<pre class="brush: php; first-line: <?=$number_of_line_start + 1?>; highlight: <?=$t['line']?>;"><?=htmlspecialchars(implode($file, "\n"))?></pre>
+				<?php endif;?>
+				
+				<pre class="brush: php;"><?=$t['args_dump']?></pre>
+			</li>
+			<?php endforeach;?>
+		</ol>
 		<?php endforeach;?>
-	</ol>
-	<?php endforeach;?>
+	</div>
 
 	<script>
 	SyntaxHighlighter.defaults.toolbar = false;
