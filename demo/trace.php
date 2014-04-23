67eb2a92af1a40035af72e3d5eddafaed778744b
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-04-23 16:42:07 +0000

    Using inline style.

--- demo/trace.php
+++ demo/trace.php
@@ -1,8 +1,16 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+ob_start();
+
+echo 'test';
+
+while (ob_get_level()) {
+    ob_end_clean();
+}
+
 function tracea () {
-    trace('a');
+    trace(time());
 }
 
 tracea();
--- src/inc/template.php
+++ src/inc/template.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="http://local:9980/gajus/2014%2001%2012%20bugger/src/inc/static/css/bugger.css">
+	<style>
+	<?=file_get_contents(__DIR__ . '/static/css/bugger.css')?>
+	<?=file_get_contents(__DIR__ . '/static/css/syntaxhighlighter/shThemeDefault.css')?>
+	<?=file_get_contents(__DIR__ . '/static/css/syntaxhighlighter/shCore.css')?>
+	</style>
 	
-	<link rel="stylesheet" type="text/css" href="http://local:9980/gajus/2014%2001%2012%20bugger/src/inc/static/css/syntaxhighlighter/shThemeDefault.css">
-	<link rel="stylesheet" type="text/css" href="http://local:9980/gajus/2014%2001%2012%20bugger/src/inc/static/css/syntaxhighlighter/shCore.css">
-
-	<script src="http://local:9980/gajus/2014%2001%2012%20bugger/src/inc/static/js/jquery-2.1.0.min.js"></script>
-	<script src="http://local:9980/gajus/2014%2001%2012%20bugger/src/inc/static/js/syntaxhighlighter/scripts/shCore.js"></script>
-	<script src="http://local:9980/gajus/2014%2001%2012%20bugger/src/inc/static/js/syntaxhighlighter/scripts/shBrushPhp.js"></script>
+	<script>
+	<?=file_get_contents(__DIR__ . '/static/js/jquery-2.1.0.min.js')?>
+	<?=file_get_contents(__DIR__ . '/static/js/syntaxhighlighter/scripts/shCore.js')?>
+	<?=file_get_contents(__DIR__ . '/static/js/syntaxhighlighter/scripts/shBrushPhp.js')?>
+	</script>
 </head>
 <body>
 	<div id="bugger" style="width: <?=count($tracestack) * 610?>px">
